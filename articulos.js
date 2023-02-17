2801d4081399e72d0926407331ce40f20257a544
Agustin Creo <agustin.creo@gmail.com>
2023-02-17 16:01:12 +0100

    Incluimos arrays y operaciones al simulador

---
+++ articulos.js
@@ -0,0 +1,48 @@
+const articulos =
+[
+    {
+        id: 1,
+        nombre: "Maceta",
+        precio: 350,
+        cantidad: 0
+    },
+
+    {
+        id: 2,
+        nombre: "Taza",
+        precio: 450,
+        cantidad: 0,
+
+    },
+
+    {
+        id: 3,
+        nombre: "Animalitos",
+        precio: 600,
+        cantidad: 0,
+
+    },
+
+    {
+        id: 4,
+        nombre: "Bowl",
+        precio: 700,
+        cantidad: 0,
+
+    },
+
+    {
+        id: 5,
+        nombre: "Plato",
+        precio: 750,
+        cantidad: 0,
+    }
+
+
+
+
+
+
+
+
+]
--- main.js
+++ main.js
@@ -1,54 +1,99 @@
+const carrito = []
+
 alert('Bienvenidos a Lunas de Valencia!')
 
-const comprarArticulos = () => {
-    let articulo = ''
-    let cantidad = 0
-    let precio = 0
-    let subtotal = 0
-    let = seguirComprando = false
+
+
+const ordenarMenorMayor = () => {
+
+    articulos.sort((a, b) => a.precio - b.precio)
+    MostrarListaArticulos()
+};
+
+const MostrarListaArticulos = () => {
+    const ListaArticulos = articulos.map(articulo => {
+        return '* '+articulo.nombre+' '+articulo.precio+'€'
+    })
+    alert('Lista de Artículos:'+'\n\n'+ListaArticulos.join('\n'))
+    comprarArticulos(ListaArticulos)
+};
+
+const comprarArticulos = (ListaArticulos) => {
+    let articuloNombre = '';
+    let articuloCantidad = 0;
+    let otroArticulo;
 
     do {
-        producto = prompt('¿Querés comprar una maceta, taza o animalitos?')
-        cantidad = parseInt(prompt('¿Cuántos querés comprar?'))
+        articuloNombre = prompt('¿Que artículo le gustaria comprar?'+'\n\n'+ListaArticulos.join('\n'))
+        articuloCantidad = parseInt(prompt('Cuántos querés comprar?'))
 
-        switch (producto) {
-            case 'maceta':
-                precio = 550               
-                break;
-            case 'taza':
-                precio = 400
-                break;
-            case 'animalitos':
-                precio = 750
-                break;        
-            default:
-                alert('Alguno de los datos son incorrectos')
-                precio = 0
-                cantidad = 0
-        }
+        const articulo = articulos.find(articulo => articulo.nombre.toLowerCase() === articuloNombre.toLowerCase());
 
-        subtotal += precio * cantidad
-        seguirComprando = confirm('¿Querés seguir comprando?')
+        // if (articulo) {
+        //     agregarAlCarrito(articulo, articulo.id, articuloCantidad)
+        // } else {
+        //     alert('El artículo no se encuentra en el catálogo.')
+        // }
 
-    } while (seguirComprando);
+               
+        otroArticulo = confirm('¿Te gustaría agregar otro artículo?')
+        
+    } while (otroArticulo);
 
-    return subtotal
+    confirmarCompra()
 
-}
+};
 
-const aplicarDscto = (subtotal) => {
-    if (subtotal >= 900) {
-        const totalConDscto = subtotal * 0.90
-        return totalConDscto
+const agregarAlCarrito = (articulo, articuloId, articuloCantidad) => {
+    const articuloDuplicado = carrito.find(articulo => articulo.id === articuloId)
+    if(!articuloDuplicado) {
+        articulo.cantidad += articuloCantidad
+        carrito.push(articulo)
     } else {
-        return subtotal
+        articuloDuplicado.cantidad += articuloCantidad;
     }
+
 }
 
-const subtotal = comprarArticulos()
+const eliminarArticuloCarrito = (articuloNombre) => {
+    carrito.forEach((articulo, index) => {
+        if (articulo.nombre.toLowerCase() === articuloNombre) {
+            if (articulo.cantidad > 1) {
+                articulo.cantidad--
+            } else {
+                carrito.splice(index, 1);
+            }
+        }
+    });
+    confirmarCompra()
+};
 
-const subtotalConDscto = aplicarDscto(subtotal)
+const confirmarCompra = () => {
+    const ListaArticulos = carrito.map(articulo => {
+        return '- '+articulo.nombre+' | Cantidad: '+articulo.cantidad
+    });
 
+    const confirmar = confirm('Resúmen de su compra: '
+    +'\n\n'+ListaArticulos.join('\n')
+    +'\n\nPara continuar presione "Aceptar" sino "Cancelar" para elimininar articulos del carrito.')
 
+    if (confirmar) {
+        finalizarCompra(ListaArticulos)
+    } else {
+        const articuloAEliminar = prompt('Ingrese el nombre del articulo que desea eliminar:')
+        eliminarArticuloCarrito(articuloAEliminar)
+    }
+};
 
+const finalizarCompra = (ListaArticulos) => {
+    const cantidadTotal = carrito.reduce((acc, elemento) => acc + elemento.cantidad, 0)
+    const precioTotal = carrito.reduce((acc, elemento) => acc + (elemento.precio * elemento.cantidad), 0)
+    alert('Aqui tiene el detalle de su compra:'
+    +'\n\n'+ListaArticulos.join('\n')
+    +'\n\nTotal de Artículos: '+cantidadTotal
+    +'\n\nEl total de su compra es: €'+precioTotal
+    +'\n\nGracias por su confianza!'
+    )
+}
 
+ordenarMenorMayor()
